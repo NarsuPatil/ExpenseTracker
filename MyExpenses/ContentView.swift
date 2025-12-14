1c00ea92c683edebc5033b6ee4f394353d7fbff5
Narsu <narsupatil1992@gmail.com>
2025-12-14 19:05:42 +0530

    Initial Commit

---
+++ MyExpenses/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyExpenses
+//
+//  Created by Narsu Patil on 14/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
